339b2d2a1ae5cd83be7c29c1f9c2c86995f84a65
P. Christopher Bowers <pcbowers@gmail.com>
2021-01-26 22:54:20 -0500

    moved and added asdf plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,50 @@
 #!/usr/bin/env bash
 
-sudo apt-add-repository ppa:neovim-ppa/stable -y
 sudo apt update -y && sudo apt upgrade -y
 
-sudo apt install git curl fish neovim -y
+sudo apt install git curl unzip python3-pip fish -y
 
 [ ! "$SHELL" = "$(which fish)" ] && chsh -s $(which fish) && echo "Default Shell set to Fish. Run \`su - $USER\` to make your updates."
-
-! command -v starship > /dev/null && curl -fsSL https://starship.rs/install.sh | bash
 
 [ ! -d "$HOME/.asdf" ] && git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
 mkdir -p ~/.config/fish/completions && cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
+if ! command -v node > /dev/null; then
+  asdf plugin add nodejs
+  bash -c '${ASDF_DATA_DIR:=$HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring'}
+  asdf install nodejs lts
+  asdf global nodejs $(asdf list nodejs | head -n 1)
+fi
+
+if ! command -v java > /dev/null; then
+  asdf plugin add java
+  asdf install java adoptopenjdk-11.0.10+9
+  asdf global java $(asdf list java | head -n 1)
+fi
+
+if ! command -v bat > /dev/null; then
+  asdf plugin add bat
+  asdf install bat latest
+  asdf global bat $(asdf list bat | head -n 1)
+fi
+
+if ! command -v starship > /dev/null; then
+  asdf plugin add starship
+  asdf install starship latest
+  asdf global starship $(asdf list starship | head -n 1)
+fi
+
 if ! command -v lsd > /dev/null; then
-	curl -sLO https://github.com/Peltoche/lsd/releases/download/0.18.0/lsd_0.18.0_amd64.deb
-	sudo apt install -y ./lsd_0.18.0_amd64.deb
-	sudo rm lsd_0.18.0_amd64.deb
+  curl -sLO https://github.com/Peltoche/lsd/releases/download/0.18.0/lsd_0.18.0_amd64.deb
+  sudo apt install -y ./lsd_0.18.0_amd64.deb
+  sudo rm lsd_0.18.0_amd64.deb
 fi
 
 if ! command -v nvim > /dev/null; then
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-	nvim --headless +PlugInstall +qall
+  asdf plugin add neovim
+  asdf install neovim latest
+  asdf global neovim $(asdf list neovim | head -n 1)
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  nvim --headless +PlugInstall +qall
 fi
--- config.fish
+++ config.fish
@@ -1,3 +1,18 @@
-starship init fish | source
+set fish_greeting
 
 source ~/.asdf/asdf.fish
+starship init fish | source
+
+abbr -a -g cat bat
+abbr -a -g vi nvim
+abbr -a -g v nvim
+abbr -a -g vim nvim
+abbr -a -g python python3
+abbr -a -g pip pip3
+abbr -a -g e explorer.exe
+abbr -a -g ls lsd
+abbr -a -g ifconfig ip addr show
+abbr -a -g c code
+abbr -a -g dropcache "sudo sh -c \"echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'\""
+abbr -a -g sf source $HOME/.config/fish/config.fish
+abbr -a -g win cd /mnt/c/Users/pcbow
